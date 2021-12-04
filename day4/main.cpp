851fcea6bb98f92e1c53acc205ccfd75ba7c45cb
nicelgueta <nicelgueta@gmail.com>
2021-12-04 14:50:42 +0000

    day3 - bit messy. should refactor

---
+++ day4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <bitset>
+
+#include "../utils.h"
+
+using namespace std;
+
+
+int main()
+{
+    
+}
--- utils.h
+++ utils.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <variant>
+#include <functional>
 
 
 namespace common
@@ -60,5 +61,20 @@ namespace common
             inputItem.substr(distanceBeginIndex)
         };
 
+    }
+
+    std::vector<std::string> pythonStyleFilter(
+        std::function<bool (std::string, int matchCriteria, int charIndex)> mappedFunc,
+        std::vector<std::string> vect,
+        int matchCriteria,
+        int charIndex
+    ){
+        std::vector<std::string> itemsToKeep;
+        for (int i = 0; i < vect.size(); i++){
+            if (mappedFunc(vect[i], matchCriteria, charIndex)){
+                itemsToKeep.push_back(vect[i]);
+            }
+        }
+        return itemsToKeep;
     }
 }
