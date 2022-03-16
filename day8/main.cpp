5f20bda0ef1c5df4ddf6878e114f4136e0171283
nicelgueta <nicelgueta@gmail.com>
2022-03-16 01:03:33 +0000

    done day8 part 1

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,15 +1,16 @@
 #include <vector>
 #include "../utils.h"
 #include <iostream>
+#include <algorithm>
 
 
 using namespace std;
 
-bool TEST = true;
+bool TEST = false;
 
 
 vector<pair<vector<string>,vector<string>>> getInputs () {
-    string fileName = TEST ? "test_input.txt" : "inputs.txt";
+    string fileName = TEST ? "test_input.txt" : "input.txt";
     vector<string> inputs = common::readInputs(fileName);
     vector<pair<vector<string>, vector<string>>> inputSplits = {};
     for (string item: inputs){
@@ -28,7 +29,27 @@ int main ()
     
     // first part - how many times do 1 (2), 4(4), 7(3), 8(7) appear
 
+    vector<int> uniques = {2,4,3,7};
+
     vector<pair<vector<string>,vector<string>>> inputs = getInputs();
+    int times = 0;
+    for (pair<vector<string>,vector<string>> combs: inputs){
+        vector<string> outs = combs.second;
+        for (string set: outs){
+            if (
+                std::find(
+                    uniques.begin(), 
+                    uniques.end(), 
+                    set.length()
+                ) != uniques.end()
+            ){
+                times++;
+            } else {
+                // nowt
+            }
+        }
+    }
+    cout << "found " << times << " times" << endl;
 
 
 }
